4d6cf8a3c63b84990fc1b927418acbdc6a4855d3
nick <n.vanderburgt@student.fontys.nl>
2020-10-09 11:31:40 +0200

    added absolute imports ability, cleaned router code

---
+++ config-fe/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    render() {
+        return(
+            <div>test</div>
+        )
+    }
+}
+
+export default Dashboard
