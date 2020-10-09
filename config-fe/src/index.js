d6d26058e41569cccbdb8c25a8a9767bbf9cbb03
nick <n.vanderburgt@student.fontys.nl>
2020-10-09 12:22:38 +0200

    added base url for imports, created side bar
    side bar can async load icons given the name in the sidebaritems.json

--- config-fe/src/index.js
+++ config-fe/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import store from 'store/store'
 import Routes from 'router/routes'
 
+import 'fontsource-roboto';
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
