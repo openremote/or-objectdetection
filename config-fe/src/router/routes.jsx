ac191c2b866d32390c367c7edcb0fb2e6cacaaaa
Bogatom <bogatom98@gmail.com>
2020-10-27 11:27:49 +0100

    Login screen
    Added an clickable login screen, there is no functionality yet

--- config-fe/src/router/routes.jsx
+++ config-fe/src/router/routes.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Dashboard from "pages/Dashboard";
 import Livefeed from "pages/Livefeed";
 import Configuration from "pages/Configuration";
+import Login from "pages/Login";
 import NotFound from "pages/NotFound";
 
 function Routes() {
@@ -12,6 +13,7 @@ function Routes() {
             <Route path="/" exact component={Dashboard} />
             <Route path="/feed/:id" exact component={Livefeed} />
             <Route path="/configuration" component={Configuration} />
+            <Route path="/Login" component={Login} />
             <Route component={NotFound} />
         </Switch>
     )
