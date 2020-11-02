e0b98ae166507d204528efee422ef4267a00f278
nick <n.vanderburgt@student.fontys.nl>
2020-11-02 11:50:01 +0100

    setup SQLalchemy ORM

---
+++ back-end/database/models/configuration.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from ..init_db import Base
+
+class Configuration(Base):
+    __tablename__ = 'configurations'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    email = Column(String(120), unique=True)
+
+    def __init__(self, name=None, email=None):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Configuration %r>' % (self.name)
