04b13dd305942b5d4e22e3cda25bedb6e85f46ea
nick <n.vanderburgt@student.fontys.nl>
2020-11-02 14:21:06 +0100

    Added proper GET for resources
    The resources have been split in two for single GET/PUT/DELETE requests and list GET and POST requests, since flask cant seperate these with routing properly, so we have to manually define two seperate resources for this.

--- back-end/resources/configuration.py
+++ back-end/resources/configuration.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, abort
 from flask_restful import Resource
 from database.models.configuration import Configuration as Conf
 
-class Configuration(Resource):
+
+class ConfigurationListAPI(Resource):
     def get(self):
         return Conf.query.all()
+
+
+class ConfigurationAPI(Resource):
+    def get(self, config_ID):
+        if config_ID is None:
+            return abort(400, description="missing required parameter")
+        else:
+            configuration = Conf.query.filter_by(id=config_ID).first()
+            if configuration is None:
+                abort(404, description=f"Configuration {config_ID} not found")
+            else:
+                return configuration
--- back-end/resources/feed.py
+++ back-end/resources/feed.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, abort
 from flask_restful import Resource
 from database.models.feed import Feed
 
-class VideoFeed(Resource):
+
+class VideoFeedListAPI(Resource):
     def get(self):
         return Feed.query.all()
+
+
+class VideoFeedAPI(Resource):
+    def get(self, feed_ID):
+        if feed_ID is None:
+            return abort(400, description="missing required parameter")
+        else:
+            configuration = Feed.query.filter_by(id=feed_ID).first()
+            if configuration is None:
+                abort(404, description=f"Feed {feed_ID} not found")
+            else:
+                return configuration
