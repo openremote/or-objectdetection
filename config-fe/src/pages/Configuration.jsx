a8376f62c211e9a41221c7f5a996a5a93df14fac
Bogatom <bogatom98@gmail.com>
2020-11-24 13:52:32 +0100

    Changes frontend configuration

--- config-fe/src/pages/Configuration.jsx
+++ config-fe/src/pages/Configuration.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import { Grid, Typography } from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Button from '@material-ui/core/Button';
+import { Grid, Typography, Card, TextField, FormGroup, FormControlLabel, Checkbox, Button, Chip, CardContent, FormControl, MenuItem, InputLabel, Box } from '@material-ui/core';
 import ReactPlayer from 'react-player';
-import Chip from '@material-ui/core/Chip';
 
 const Configuration = (props) => {
     const classes = useStyles();
@@ -54,92 +45,126 @@ const Configuration = (props) => {
                     <Typography variant="h3" className={classes.title}>
                         Configuratie - Camera X
                     </Typography>
-                    <InputLabel className={classes.inputTitle}><b>Display name:</b></InputLabel>
-                    <FormControl className={classes.formControl}>
-                        <TextField size="normal" id="standard-basic" value="Kruispunt XY" />
-                    </FormControl>
 
-                    <InputLabel className={classes.inputTitle}><b>Selecteer Detecties</b></InputLabel>
-                    <FormControl className={classes.formControl}>
-                        <TextField select size="normal" value="" onChange={e => handleSelect(e)}>
-                            {detectionOptions.map(option => (
-                                <MenuItem key={option.key} value={option.label}>
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                        <ul className={classes.chipControl}>
-                            {
-                                chipData.map((data, index) => {
-                                    return (
-                                        <Chip
-                                            className={classes.chip}
-                                            key={data.key + index}
-                                            label={data.label}
-                                            onDelete={handleDelete(data)}
-                                            color="primary"
+                    <Card className={classes.root}>
+                        <CardContent className={classes.cardContent}>
+                            <Box className={classes.box}>
+                                <div style={{ display: "inline-block" }}>
+                                    <InputLabel className={classes.inputTitle}><b>Naam:</b></InputLabel>
+                                    <Typography className={classes.title2} color="textSecondary" gutterBottom>
+                                        <TextField size="normal" id="standard-basic" value="Kruispunt XY" />
+                                    </Typography>
+                                </div>
+                                <div style={{ display: "inline-block" }}>
+                                    <InputLabel className={classes.inputTitle}><b>Locatie:</b></InputLabel>
+                                    <Typography className={classes.title2} color="textSecondary" gutterBottom>
+                                        <TextField size="normal" id="standard-basic" value="Strijp S" />
+                                    </Typography>
+                                </div>
+                                <div style={{ display: "inline-block" }}>
+                                    <InputLabel className={classes.inputTitle}><b>Type:</b></InputLabel>
+                                    <Typography className={classes.title2} color="textSecondary" gutterBottom>
+                                        <TextField size="normal" id="standard-basic" value="Camera C920" disabled />
+                                    </Typography>
+                                </div>
+                                <div style={{ display: "inline-block" }}>
+                                    <InputLabel className={classes.inputTitle}><b>Resolutie:</b></InputLabel>
+                                    <Typography className={classes.title2} color="textSecondary" gutterBottom>
+                                        <TextField size="normal" id="standard-basic" value="1920x1080" disabled />
+                                    </Typography>
+                                </div>
+                                <div style={{ display: "inline-block" }}>
+                                    <InputLabel className={classes.inputTitle}><b>Framerate:</b></InputLabel>
+                                    <Typography className={classes.title2} color="textSecondary" gutterBottom>
+                                        <TextField size="normal" id="standard-basic" value="30fps" disabled />
+                                    </Typography>
+                                </div>
+                                <InputLabel className={classes.inputTitle}><b>Url:</b></InputLabel>
+                                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                    <TextField className={classes.textField} size="normal" id="standard-basic" value="https://www.openremote.com/videofeed/kruispunt" />
+                                </Typography>
+                                <InputLabel className={classes.inputTitle}><b>Selecteer Detecties</b></InputLabel>
+                                <TextField className={classes.comboField} select size="normal" value="" onChange={e => handleSelect(e)}>
+                                    {detectionOptions.map(option => (
+                                        <MenuItem key={option.key} value={option.label}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                                <ul className={classes.chipControl}>
+                                    {
+                                        chipData.map((data, index) => {
+                                            return (
+                                                <Chip
+                                                    className={classes.chip}
+                                                    key={data.key + index}
+                                                    label={data.label}
+                                                    onDelete={handleDelete(data)}
+                                                    color="primary"
+                                                />
+                                            );
+                                        })
+                                    }
+                                </ul>
+                                <InputLabel className={classes.inputTitle}><b>Detectie Opties</b></InputLabel>
+                                <FormControl component="fieldset" className={classes.formControl}>
+                                    <FormGroup>
+                                        <FormControlLabel
+                                            control={<Checkbox checked={D} onChange={handleChange} name="D" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Directions"
                                         />
-                                    );
-                                })
-                            }
-                        </ul>
-                    </FormControl>
-                    <InputLabel className={classes.inputTitle}><b>Detectie Opties</b></InputLabel>
-                    <FormControl component="fieldset" className={classes.formControl}>
-                        <FormGroup>
-                            <FormControlLabel
-                                control={<Checkbox checked={D} onChange={handleChange} name="D" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Directions"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox checked={BB} onChange={handleChange} name="BB" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Boundary Boxes"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox checked={CLC} onChange={handleChange} name="CLC" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Calculate Line Cross"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox checked={CO} onChange={handleChange} name="CO" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Count Objects"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox checked={SOS} onChange={handleChange} name="SOS" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Show object speed"
-                            />
-                            <FormControlLabel
-                                control={<Checkbox checked={VC} onChange={handleChange} name="VC" style={{
-                                    color: "#4D9D2A",
-                                }} />}
-                                label="Visualize Centers"
-                            />
-                            <Button variant="contained" color="primary" className={classes.Buttons}>
-                                Save
-                        </Button>
-                            <Button variant="contained" color="default" className={classes.Buttons}>
-                                Cancel
-                        </Button>
-                        </FormGroup>
-                    </FormControl>
+                                        <FormControlLabel
+                                            control={<Checkbox checked={BB} onChange={handleChange} name="BB" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Boundary Boxes"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox checked={CLC} onChange={handleChange} name="CLC" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Calculate Line Cross"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox checked={CO} onChange={handleChange} name="CO" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Count Objects"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox checked={SOS} onChange={handleChange} name="SOS" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Show object speed"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox checked={VC} onChange={handleChange} name="VC" style={{
+                                                color: "#4D9D2A",
+                                            }} />}
+                                            label="Visualize Centers"
+                                        />
+                                        <Button variant="contained" color="primary" className={classes.Buttons}>
+                                            Save
+                                        </Button>
+                                        <Button variant="contained" color="default" className={classes.Buttons}>
+                                            Cancel
+                                        </Button>
+                                    </FormGroup>
+                                </FormControl>
+                            </Box>
+                        </CardContent>
+                    </Card>
                 </Grid>
                 <Grid item xs={6}>
                     <Typography variant="h3" className={classes.title}>
                         Camera Preview
                     </Typography>
-                    <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
+                    <ReactPlayer url='https://www.youtube.com/watch?v=njCzFI149Rc' />
                 </Grid>
             </Grid>
-        </div>
+        </div >
     )
 }
 
@@ -148,21 +173,30 @@ export default Configuration;
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
-        minWidth: '50%',
-        display: ''
+        width: '100%',
+        display: "flex",
     },
     selectEmpty: {
         marginTop: theme.spacing(2),
     },
     title: {
         color: '#4D9D2A',
-        marginBottom: '20px'
+        marginBottom: '20px',
+    },
+    title2: {
+        color: '#4D9D2A',
+        marginBottom: '20px',
+        width: "100%",
+        display: "inline-block",
+        marginRight: 40,
     },
     inputTitle: {
-        marginTop: '30px'
+        marginTop: '30px',
+        width: "40%"
     },
     Buttons: {
-        marginTop: '10px'
+        marginTop: '10px',
+        width: "100%"
     },
     chipControl: {
         display: 'flex',
@@ -178,5 +212,21 @@ const useStyles = makeStyles((theme) => ({
     },
     inputField: {
         height: 80,
+    },
+    box: {
+        margin: "10px",
+        width: "100%",
+    },
+    root: {
+        width: "100%",
+    },
+    textField: {
+        width: "100%",
+    },
+    comboField: {
+        width: "50%",
+    },
+    cardContent: {
+        display: "flex"
     }
 }));
