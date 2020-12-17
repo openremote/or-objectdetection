95bf578646a39cc8d67f8f5559ce84dbd8de99ce
Tygo Driessen <tygo.driessen@student.fontys.nl>
2020-12-17 13:38:29 +0100

    Remove video feed worker

--- back-end/workers/init_worker.py
+++ back-end/workers/init_worker.py
@@ -1,25 +1,7 @@
 import os
 import random
 
-from kombu import Connection, Exchange, Queue
-from workers.VideoFeedWorker import Worker
 from workers.VideoDetectionWorker import VideoDetectionWorker
-
-# Default RabbitMQ server URI
-rabbit_url = 'amqp://rabbitmq:rabbitmq@192.168.99.100:5672//'
-
-
-# starts the rabbitMQ consumer worker, which will read all rabbitmq video feed messages and pass them to a websocket
-# server, this way we can make sure the backend is responsible for the consuming of the queue and not the front end
-# using amqplib
-def startConsumingWorker():
-    print("starting consuming worker.....")
-    exchange = Exchange("video-exchange", type="direct")
-    queues = [Queue("video-queue", exchange, routing_key="video")]
-    with Connection(rabbit_url, heartbeat=4) as conn:
-        worker = Worker(conn, queues)
-        worker.start()
-        #worker.run()
 
 def startVideoDetectionWorker():
     broker = os.getenv("MQTT_HOST", "127.0.0.1")
