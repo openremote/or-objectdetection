39e648ca2f77dd729eac517ce4b0ce50d36a3867
Tygo Driessen <tygo.driessen@student.fontys.nl>
2020-12-17 13:21:22 +0100

    Add videoDetectionWorker

---
+++ back-end/workers/VideoDetectionWorker.py
@@ -0,0 +1,38 @@
+import time
+import threading
+
+from paho.mqtt import client as mqtt_client
+
+class VideoDetectionWorker(threading.Thread):
+    def __init__(self, broker, port, client_id):
+        self.connect_mqtt(broker, port, client_id)
+
+        super(VideoDetectionWorker, self).__init__()
+
+    def connect_mqtt(self, broker, port, client_id):
+        def run(self):
+            super(VideoDetectionWorker, self).run()
+
+        def on_connect(self, client, userdata, flags, rc):
+            if rc == 0:
+                print("Connected to MQTT Broker!")
+            else:
+                print("Failed to connect, return code %d\n", rc)
+    
+        client = mqtt_client.Client(client_id)
+        client.on_connect = on_connect
+        client.connect(broker, port) # TODO: Add support for brokers with authentication
+        return client
+
+    def publish(self, client):
+        msg_count = 0
+        while True:
+            time.sleep(1)
+            msg = f"messages: {msg_count}"
+            result = client.publish("/python/mqtt", msg)
+            status = result[0]
+            if status == 0:
+                print(f"Send `{msg}` to topic `/python/mqtt`")
+            else:
+                print(f"Failed to send message to topic /python/mqtt")
+            msg_count += 1
--- back-end/workers/init_worker.py
+++ back-end/workers/init_worker.py
@@ -1,6 +1,9 @@
-import eventlet
+import os
+import random
+
 from kombu import Connection, Exchange, Queue
 from workers.VideoFeedWorker import Worker
+from workers.VideoDetectionWorker import VideoDetectionWorker
 
 # Default RabbitMQ server URI
 rabbit_url = 'amqp://rabbitmq:rabbitmq@192.168.99.100:5672//'
@@ -17,3 +20,12 @@ def startConsumingWorker():
         worker = Worker(conn, queues)
         worker.start()
         #worker.run()
+
+def startVideoDetectionWorker():
+    broker = os.getenv("MQTT_HOST", "127.0.0.1")
+    port = os.getenv("MQTT_PORT", 1883)
+    client_id = os.getenv("MQTT_CLIENT_ID", f'python-mqtt-{random.randint(0, 1000)}')
+
+    print("Starting videoDetectionWorker.....")
+    worker = VideoDetectionWorker(broker, port, client_id)
+    worker.start()
