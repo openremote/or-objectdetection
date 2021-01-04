04c7c684594096281392f3d62fe0100709c109c9
Tygo Driessen <tygo.driessen@student.fontys.nl>
2021-01-04 11:55:52 +0100

    Better connection handling and authentication support

--- back-end/workers/MQTTWorker.py
+++ back-end/workers/MQTTWorker.py
@@ -4,35 +4,57 @@ import threading
 from paho.mqtt import client as mqtt_client
 
 class MQTTWorker(threading.Thread):
-    def __init__(self, broker, port, client_id):
-        self.connect_mqtt(broker, port, client_id)
+    def __init__(self, broker, port, client_id, username, password):
+        self.connect_mqtt(broker, port, client_id, username, password)
 
         super(MQTTWorker, self).__init__()
 
-    def connect_mqtt(self, broker, port, client_id):
+    def connect_mqtt(self, broker, port, client_id, username, password):
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
+                client.connected_flag = True
                 print("Connected to MQTT Broker!")
             else:
-                print("Failed to connect, return code %d\n", rc)
+                print("Failed to connect, returned code =", rc)
+                client.bad_connection_flag=True
+
+        def on_disconnect(client, userdata, rc):
+            print("disconnecting reason "  +str(rc))
+            client.connected_flag=False
+            client.disconnect_flag=True
 
         # Setup an MQTT client
+        mqtt_client.Client.connected_flag=False
+        mqtt_client.Client.bad_connection_flag=False
         client = mqtt_client.Client(client_id)
         client.on_connect = on_connect
+        client.on_disconnect = on_disconnect
+        client.loop_start()
+        if username and password:
+            client.username_pw_set(username, password)
 
-        # Try to connect to the specified MQTT server
         try:
-            client.connect(broker, int(port)) # TODO: Add support for brokers with authentication
+            client.connect(broker, int(port))
         except:
-            print("MQTT Connection failed")
+            print("connection failed")
             exit(1)
+        while not client.connected_flag and not client.bad_connection_flag:
+            print("In wait loop")
+            time.sleep(1)
+        if client.bad_connection_flag:
+            client.loop_stop()
+            exit(1)
+        print("in Main loop")
+        client.publish("openremote/od", "YEET")
+        client.loop_stop()
+        client.disconnect()
 
         return client
 
     def publish(self, client):
         msg_count = 0
         while True:
-            time.sleep(1)
+            time.sleep(5)
             msg = f"messages: {msg_count}"
             result = client.publish("/python/mqtt", msg)
             status = result[0]
--- back-end/workers/init_worker.py
+++ back-end/workers/init_worker.py
@@ -7,10 +7,12 @@ def startMQTTWorker():
     broker = os.getenv("MQTT_HOST")
     port = os.getenv("MQTT_PORT", 1883)
     client_id = os.getenv("MQTT_CLIENT_ID", f'python-mqtt-{random.randint(0, 1000)}')
+    username = os.getenv("MQTT_USERNAME")
+    password = os.getenv("MQTT_PASSWORD")
 
     if not broker:
         print("No MQTT host spcified. MQTT disabled.")
     else:
         print("Starting MQTTWorker.....")
-        worker = MQTTWorker(broker, port, client_id)
+        worker = MQTTWorker(broker, port, client_id, username, password)
         worker.start()
