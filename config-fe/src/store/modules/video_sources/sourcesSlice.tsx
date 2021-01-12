27453b5f9b453caa481ad1b97ee85ed38f088918
nick <n.vanderburgt@student.fontys.nl>
2021-01-12 12:28:08 +0100

    Added remove feed option, livefeed doesnt attempt to connect if inactive

--- config-fe/src/store/modules/video_sources/sourcesSlice.tsx
+++ config-fe/src/store/modules/video_sources/sourcesSlice.tsx
@@ -22,8 +22,11 @@ export const sourcesSlice = createSlice({
       },
       RemoveSource: (state, action : PayloadAction<Feed>) => {
         //delete from local state array.
-        var indexOfVideoToDelete = state.videoSources.indexOf(action.payload);
-        state.videoSources.splice(indexOfVideoToDelete, 1);
+        let feed = state.videoSources.findIndex(x => x.id === action.payload?.id);
+        if(feed !== -1) {
+          console.log(feed);
+          state.videoSources.splice(feed, 1);
+        }
       },
       ChangeFeedActive: (state, action : PayloadAction<Feed>) => {
         var foundIndex = state.videoSources.findIndex(x => x.id == action.payload.id);
